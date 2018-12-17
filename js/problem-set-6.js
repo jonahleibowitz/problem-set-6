7e6551dba82c6bafa0ce6e1e76f2dedbf5e32abb
jonahleibowitz <34136455+jonahleibowitz@users.noreply.github.com>
2018-12-17 17:59:46 -0500

    working on star

--- js/problem-set-6.js
+++ js/problem-set-6.js
@@ -270,22 +270,50 @@ ctx.stroke();
 
 function drawStar() {
 
-  var c=document.getElementById("canvas6");
-  var ctx=c.getContext("2d");
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
+let canvas = document.getElementById('canvas6');
+    let ctx = canvas.getContext('2d');
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-let outerRad= Number(prompt("Choose outer radius."));
-let innerRad= Number(prompt("Choose inner radius."));
+  
+let radiusOut= Number(prompt("Enter valid outer radius."));
+let radiusIn= Number(prompt("Enter valid inner radius."));
+  if (radiusOut>radiusIn && canvas.width>=radiusOut+125 && canvas.height>=radiusOut+125 && radiusIn>0 && radiusOut>0){
+      
+ let spikes = 5;
+    
+     function drawStar(cx,cy,spikes,radiusOut,radiusIn){
+      var rot=Math.PI/2*3;
+      var x=cx;
+      var y=cy;
+      var step=Math.PI/spikes;
 
-ctx.beginPath();
-ctx.fillStyle = "#C40043";
-ctx.arc(100, 100, 100, 0, Math.PI * 2);
-ctx.fill();
+      ctx.beginPath();
+      ctx.moveTo(cx,cy-radiusOut)
+      for(i=0;i<spikes;i++){
+        x=cx+Math.cos(rot)*radiusOut;
+        y=cy+Math.sin(rot)*radiusOut;
+        ctx.lineTo(x,y)
+        rot+=step
 
-ctx.fillStyle = "yellow";
-drawStar(ctx, 100, 100, 9, 90, 50);
-ctx.fill();
+        x=cx+Math.cos(rot)*radiusIn;
+        y=cy+Math.sin(rot)*radiusIn;
+        ctx.lineTo(x,y)
+        rot+=step
+      }
+      ctx.lineTo(cx,cy-radiusOut);
+      ctx.closePath();
+      ctx.lineWidth=5;
+      ctx.strokeStyle='blue';
+      ctx.stroke();
+      ctx.fillStyle='skyblue';
+      ctx.fill();
+    }
 
+    drawStar(125,125,5,30,15);
+    
+    
+    
+  }
 }
 
 /*
